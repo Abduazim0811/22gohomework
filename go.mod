a61827a8489a3559085e9f2d1c36818bb1fd9304
Abduazim <yusupovabduazim0811@gmail.com>
2024-04-26 00:06:36 +0500

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Abduazim0811/22gohomework
+
+go 1.22.1
--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "./git"
+	"fmt"
+	"os"
+
+	git "github.com/Abduazim0811/22gohomework/repositer"
 )
 
 func main() {
-    username, err := git.GetUserName()
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    file, err := os.Create("data.txt")
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer file.Close()
-    file.WriteString(username + "\n")
+	username, err := git.GetUserName()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	file, err := os.Create("data.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+	file.WriteString(username + "\n")
 }
